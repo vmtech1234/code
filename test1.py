4f24cf16188f4b8040a3ac2f57d35c4ff80d7bef
vmtech1234 <67905055+vmtech1234@users.noreply.github.com>
2020-07-06 16:58:05 +0530

    for upload
    changed

---
+++ test1.py
@@ -0,0 +1,52 @@
+from selectorlib import Extractor
+import requests 
+import json
+import csv 
+from time import sleep
+
+
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('/Users/vengatrvs/Desktop/3.txt')
+
+def scrape(url):  
+
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.topdoctors.mx/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8;U+00C9',
+    }      
+    # Download the page using requests
+    #print("JOB STARTED")
+    
+    print("Scraping %s"%url)
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Pass the HTML of the page and create 
+    return e.extract(r.text)
+
+
+# product_data = []
+
+with open("/Users/vengatrvs/Desktop/python/inputs/urls.txt",'r') as urllist, open('/Users/vengatrvs/Desktop/PYTHON/OUTPUTS/nw.csv','w') as outfile:
+    for url in urllist.read().splitlines():
+        data = scrape(url) 
+        if data:
+            json.dump(data,outfile)
+            outfile.write("," +url)
+            outfile.write("\n")
+            #print("File Experted !")
+            # sleep(5)
